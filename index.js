1b75f4aef2ad8135fad94ed6d9424afbb100349b
Daniel Barczyński <daniel.barczynski@onet.pl>
2022-10-02 12:03:27 +0200

    rozgrzewka

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log("hello world")
+
+// rozgrzewka: 4 pola tekstowe w htmlu
+// dopiac do tego js, przycisk przelicz (min max srednia i sume)
+
+const calcButton = document.getElementsByClassName("calc");
+const i1 = document.getElementsByClassName("i1");
+const i2 = document.getElementsByClassName("i2");
+const i3 = document.getElementsByClassName("i3");
+const i4 = document.getElementsByClassName("i4");
+const scores = document.getElementsByClassName("scores");
+
+calcButton.addEventListener("click", function () {
+    const sum = i1.value + i2.value + i3.value + i4.value;
+    scores.textContent = sum;
+});
