8d89a5926dcf04bee16e66553cacb498b9d3a710
Daniel Barczyński <daniel.barczynski@onet.pl>
2022-10-05 20:13:11 +0200

    rozgrzewka done

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-console.log("hello world")
+'use strict';
 
-// rozgrzewka: 4 pola tekstowe w htmlu
-// dopiac do tego js, przycisk przelicz (min max srednia i sume)
+const calcButton = document.querySelector('#calc');
+const i1 = document.querySelector('#i1');
+const i2 = document.querySelector('#i2');
+const i3 = document.querySelector('#i3');
+const i4 = document.querySelector('#i4');
+const scores = document.querySelector('#scores');
 
-const calcButton = document.querySelector("#calc");
-const i1 = document.querySelector("#i1");
-const i2 = document.querySelector("#i2");
-const i3 = document.querySelector("#i3");
-const i4 = document.querySelector("#i4");
-const scores = document.querySelector("#scores");
-
-// sprobowac queryselector
-calcButton.addEventListener("click", function onClick() {
-    let array = [i1.value, i2.value, i3.value, i4.value];
-    // consoloe log array elements
-    let arrayPrint = 0;
-    for (let i = 0; i < array.length; i++) {
-        arrayPrint = array[i];
-        console.log(arrayPrint);
-    }
-    let sum = Math. i1.value + i2.value + i3.value + i4.value;
-    const average = sum/4;
-    const min = Math.min(array);
-    const max = Math.max(array);
+calcButton.addEventListener('click', function onClick() {
+    let num1 = parseInt(i1.value) || 0;
+    let num2 = parseInt(i2.value) || 0;
+    let num3 = parseInt(i3.value) || 0;
+    let num4 = parseInt(i4.value) || 0;
+    
+    let array = [num1, num2, num3, num4];
+    const sum = array.reduce((a, b) => a + b, 0); 
+    const average = array.reduce((a, b) => a + b, 0) / array.length;
+    const min = Math.min(...array);
+    const max = Math.max(...array);
     scores.textContent = `${sum} ${average} ${min} ${max}`;
 });
