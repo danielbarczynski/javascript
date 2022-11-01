bc1661853965be0002698baf2c8244081039655d
Daniel PC <daniel.barczynski@onet.pl>
2022-11-01 19:18:03 +0100

    js draft

--- main.js
+++ main.js
@@ -1,24 +1,56 @@
-// notatnik z zajęć
-const main = document.querySelector('main')
-const timeoutRef = setTimeout( 
-    () => {
-        main.innerHTML='From setTimeout'
-    },
-    2000
-)
-let licznik = 0 
-const intervalRef = setInterval( 
-    () => {
-        main.innerHTML='From interval' + licznik++
-    },
-    4000
-)
+// // notatnik z zajęć
+// const main = document.querySelector('main')
+// const timeoutRef = setTimeout( 
+//     () => {
+//         main.innerHTML='From setTimeout'
+//     },
+//     2000
+// )
+// let licznik = 0 
+// const intervalRef = setInterval( 
+//     () => {
+//         main.innerHTML='From interval' + licznik++
+//     },
+//     4000
+// )
 
-// kasujemy setInterval
-clearInterval(intervalRef)
+// // kasujemy setInterval
+// clearInterval(intervalRef)
 
-// kasujemy setTimeout
-clearTimeout(intervalRef)
+// // kasujemy setTimeout
+// clearTimeout(intervalRef)
 
 
-// window.requestAnimationFrame
+// // window.requestAnimationFrame
+
+'use strict';
+
+let index = 1;
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const img = document.querySelectorAll('img');
+
+const slide = function (num) {
+    show(index += num);
+}
+
+const show = function (num) {
+    console.log('clicked');
+    let i = 0;
+
+    if (num > img.length)
+        index = 1;
+    
+    if (num < 1)
+        index = img.length;
+
+    for (let i = 0; i < img.length; i++) {
+        img[i].style.display = 'none';    
+    }
+
+    img[index - 1].style.display = 'block';
+}
+
+show(index);
+left.addEventListener('click', show(-1));
+right.addEventListener('click', show(+1));
