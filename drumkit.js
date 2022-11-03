0eee992316cca63c30d99588112d833f589c4986
Daniel PC <daniel.barczynski@onet.pl>
2022-11-03 20:23:59 +0100

    sounds working

--- drumkit.js
+++ drumkit.js
@@ -33,13 +33,14 @@
 // // Date.now()
 'use strict';
 
-const playSound = function(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}}"]`);
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+
     if (!audio) 
         return;
-    
+        
+    audio.currentTime = 0;
     audio.play();
-    
 }
 
-window.addEventListener('keydown', playSound);
+window.addEventListener('keypress', playSound);
