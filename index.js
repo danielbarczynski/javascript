d1ce3e91bfe0503c1fc5224e89095b8ebc1440c9
Daniel PC <daniel.barczynski@onet.pl>
2022-11-24 21:50:54 +0100

    draft-done

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const title = document.querySelector('#title');
 const text = document.querySelector('#text');
 const addBtn = document.querySelector('#add');
+const rmvBtn = document.querySelector('#remove');
 const notes = document.querySelector('#notes');
 
 getItems();
 
 function addNote() {
     if (title.value === '' || text.value === '') return;
+
     localStorage.setItem(title.value, text.value);
+    refresh();
+    getItems();
+}
+
+function refresh() {
     title.value = '';
     text.value = '';
     notes.innerHTML = '';
+}
+
+//* removing by inserting title to the input. updating works the same (for now)
+function removeNote() {
+    if (title.value === '') return;
+
+    localStorage.removeItem(title.value);
+    refresh();
     getItems();
 }
 
@@ -18,8 +33,17 @@ function getItems() {
     for (let i = 0; i < localStorage.length; i++) { 
         const key = localStorage.key(i);
         const value = localStorage.getItem(key);
-        notes.innerHTML += `Title: ${key} <br /> ${value}<br /><br />`
+        const note = document.createElement('div');
+
+        note.style.display = 'block';
+        note.style.padding = '5px';
+        note.style.margin = '5px';
+        note.style.border = 'solid 1px';
+
+        notes.appendChild(note);
+        note.innerHTML = `<b>${key} </b> <br /> <br />${value}`;
     }
 }
 
 addBtn.addEventListener('click', addNote);
+rmvBtn.addEventListener('click', removeNote);
