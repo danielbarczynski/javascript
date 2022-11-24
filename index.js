8c8dee9e03f2d288eeb8cd1d977aa9a3f505dc22
Daniel PC <daniel.barczynski@onet.pl>
2022-11-24 21:15:55 +0100

    adding-draft

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-const title = document.querySelector('#title'); 
+const title = document.querySelector('#title');
 const text = document.querySelector('#text');
 const addBtn = document.querySelector('#add');
 const notes = document.querySelector('#notes');
 
+getItems();
+
 function addNote() {
+    if (title.value === '' || text.value === '') return;
     localStorage.setItem(title.value, text.value);
     title.value = '';
     text.value = '';
+    notes.innerHTML = '';
+    getItems();
 }
 
-notes.innerHTML = `${localStorage.getItem('daniel')}`
+function getItems() {
+    for (let i = 0; i < localStorage.length; i++) { 
+        const key = localStorage.key(i);
+        const value = localStorage.getItem(key);
+        notes.innerHTML += `Title: ${key} <br /> ${value}<br /><br />`
+    }
+}
+
 addBtn.addEventListener('click', addNote);
