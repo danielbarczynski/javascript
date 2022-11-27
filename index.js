f014df94873ac5334ec3750f610e03a1d9d2b771
Daniel Barczyński <daniel.barczynski@onet.pl>
2022-11-27 12:07:56 +0100

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+const addNums = (x) => {
+    for (let i = 0; i <= 50; i++) {
+        asyncAdd(i, x++);  
+    }
+}
+
+const measureTime = (x) => {
+    const t1 = performance.now();
+    addNums(x);
+    const t2 = performance.now();
+    console.log(`t1: ${t1} t2: ${t2}`);
+    console.log(t1 - t2);
+}
+
+const measureTime2 = (x) => {
+    performance.mark('start');
+    // addNums(x);
+    performance.mark('end');
+    performance.measure('start', 'end');
+}
+
+const asyncAdd = async (a, b) => {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return Promise.reject('Argumenty muszą mieć typ number!')
+    }
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 100)
+    })
+}
+
+measureTime(1);
+measureTime2(1);
