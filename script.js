b930926835a08dfc00e9338480b803a450c2e720
Daniel PC <daniel.barczynski@onet.pl>
2023-01-07 00:22:46 +0100

    circle class, multiple circles moving

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-const randomDirection = Math.floor(Math.random() < 0.5 ? -1 : 1) * 1;
-const randomX = Math.floor(Math.random() * 300);
-const randomY = Math.floor(Math.random() * 300);
 const circles = [];
 canvas.width = 500;
 canvas.height = 500;
 
-const circle = {
-    x: randomX,
-    y: randomY,
-    size: 15,
-    dx: randomDirection,
-    dy: randomDirection
+class Circle {
+    constructor(x, y, size, dx, dy) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.dx = dx;
+        this.dy = dy;
+    }
+}
+//? random color from the array?
+
+for (let i = 5; i < 15; i++) {
+    newCirle = new Circle(Math.floor(Math.random() * 300), Math.floor(Math.random() * 300), 15, Math.floor(Math.random() < 0.5 ? -1 : 1) * 1, Math.floor(Math.random() < 0.5 ? -1 : 1) * 1);
+    circles.push(newCirle);
 }
 
-for (let i = 0; i < 5; i++) {
-    circles.push(circle);
-}
 function drawCircle() {
-    i = 10;
-    circles.forEach(element => {
-        i *= 1.5;
-        ctx.arc(i, element.y, element.size, 0, Math.PI * 2);
+    circles.forEach(el => {
+        ctx.beginPath();
+        ctx.arc(el.x, el.y, el.size, 0, Math.PI * 2);
         ctx.fillStyle = 'grey';
         ctx.fill();        
+        ctx.closePath();
+        el.x += el.dx;
+        el.y += el.dy;
+
+        if (el.x + el.size > canvas.width || el.x - el.size < 0)
+            el.dx *= -1;
+        else if (el.y + el.size > canvas.height || el.y - el.size < 0)
+            el.dy *= -1;
     });
 }
 
 function update() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
     drawCircle();
-    circle.x += circle.dx;
-    circle.y += circle.dy;
-    ctx.closePath();
     requestAnimationFrame(update);
-
-    if (circle.x >= canvas.width - circle.size || circle.x <= 0 + circle.size)
-        circle.dx *= -1;
-    else if (circle.y >= canvas.height - circle.size || circle.y <= 0 + circle.size)
-        circle.dy *= -1;
 }
 
 update();
