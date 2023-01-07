505b60dd88b693da4bd3c9e690f851b059b338ac
Daniel PC <daniel.barczynski@onet.pl>
2023-01-07 15:05:44 +0100

    up

--- script.js
+++ script.js
@@ -13,10 +13,11 @@ class Circle {
         this.dy = dy;
     }
 }
-//? random color from the array?
+
+const distance = (x1, x2) => Math.hypot(x2 - x1);
 
 for (let i = 5; i < 15; i++) {
-    newCirle = new Circle(Math.floor(Math.random() * 300), Math.floor(Math.random() * 300), 15, Math.floor(Math.random() < 0.5 ? -1 : 1) * 1, Math.floor(Math.random() < 0.5 ? -1 : 1) * 1);
+    newCirle = new Circle(Math.floor(Math.random() * 300), Math.floor(Math.random() * 300), 15, Math.floor(Math.random() < 0.5 ? -1 : 1) * i / 7, Math.floor(Math.random() < 0.5 ? -1 : 1) * i / 7);
     circles.push(newCirle);
 }
 
@@ -24,17 +25,31 @@ function drawCircle() {
     circles.forEach(el => {
         ctx.beginPath();
         ctx.arc(el.x, el.y, el.size, 0, Math.PI * 2);
-        ctx.fillStyle = 'grey';
-        ctx.fill();        
+        ctx.fillStyle = 'coral';
+        ctx.fill();
         ctx.closePath();
         el.x += el.dx;
         el.y += el.dy;
+        drawLine();
 
         if (el.x + el.size > canvas.width || el.x - el.size < 0)
             el.dx *= -1;
         else if (el.y + el.size > canvas.height || el.y - el.size < 0)
             el.dy *= -1;
     });
+}
+
+function drawLine() {
+    ctx.strokeStyle = 'coral';
+    circles.forEach(el => {
+        circles.some(el2 => {
+            if (distance(el.x, el2.x) < 10 || distance(el.y, el2.y) < 10) {
+                ctx.moveTo(el.x, el.y);
+                ctx.lineTo(el2.x, el2.y);
+                ctx.stroke();
+            }
+        });
+    })
 }
 
 function update() {
